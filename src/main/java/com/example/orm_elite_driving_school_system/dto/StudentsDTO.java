8f63ddae3fb96cb041e0d05333efd87292055b86
SAMOKSHA <samokshasri1@gmail.com>
2025-09-16 21:53:40 +0530

    12th commit (Dto = Students )

---
+++ src/main/java/com/example/orm_elite_driving_school_system/dto/StudentsDTO.java
@@ -0,0 +1,32 @@
+package com.example.orm_elite_driving_school_system.dto;
+
+
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+@ToString
+@Builder
+
+public class StudentsDTO {
+    private String studentId;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phone;
+    private String address;
+    private Date dob;
+    private Date registrationDate;
+    @Builder.Default
+    private ArrayList<StudentCourseDetailsDTO> studentCourseDetails = new ArrayList<>();
+    @Builder.Default
+    private ArrayList<LessonsDTO> lessons =new ArrayList<>();
+    @Builder.Default
+    private ArrayList<PaymentsDTO> payments =new ArrayList<>();
+
+}
