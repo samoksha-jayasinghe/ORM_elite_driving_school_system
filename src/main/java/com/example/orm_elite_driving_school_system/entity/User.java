0f66752004b6b2fc51883f724da8c99b309d05af
SAMOKSHA <samokshasri1@gmail.com>
2025-09-16 15:44:38 +0530

    7th commit (Entity = user ) complete Entity

---
+++ src/main/java/com/example/orm_elite_driving_school_system/entity/User.java
@@ -0,0 +1,39 @@
+package com.example.orm_elite_driving_school_system.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+
+@Entity
+@Table(name = "user")
+public class User {
+
+    @Id
+    @Column
+    private String userId;
+
+    @Column(nullable = false, unique = true)
+    private String userName;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String role;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String status;
+}
