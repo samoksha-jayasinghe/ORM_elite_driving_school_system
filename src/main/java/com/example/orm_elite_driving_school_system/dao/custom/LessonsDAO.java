338475deceef22275264caa192ff86b72a52e77b
SAMOKSHA <samokshasri1@gmail.com>
2025-09-17 15:09:14 +0530

    15th commit (DAO= lessondDAO)

---
+++ src/main/java/com/example/orm_elite_driving_school_system/dao/custom/LessonsDAO.java
@@ -0,0 +1,7 @@
+package com.example.orm_elite_driving_school_system.dao.custom;
+
+import lk.ijse.elitedrivingschoolsystemormcoursework.dao.CrudDAO;
+import lk.ijse.elitedrivingschoolsystemormcoursework.entity.Lessons;
+
+public interface LessonsDAO extends CrudDAO<Lessons> {
+}
