3542982af8ac8594873ecd1d90dcc48389930507
SAMOKSHA <samokshasri1@gmail.com>
2025-09-22 12:12:38 +0530

    32th commit

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -8,5 +8,9 @@ module com.example.orm_elite_driving_school_system {    requires javafx.controls
 
 
     opens com.example.orm_elite_driving_school_system to javafx.fxml;
+    opens com.example.orm_elite_driving_school_system.config to jakarta.persistence;
+    opens com.example.orm_elite_driving_school_system.entity to org.hibernate.orm.core;
+    opens com.example.orm_elite_driving_school_system.controller to javafx.fxml;
+    opens com.example.orm_elite_driving_school_system.dto.tm to javafx.base;
     exports com.example.orm_elite_driving_school_system;
 }
